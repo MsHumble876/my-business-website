172219bba372bf35024ea86bf75dc25daa0ad231
MsHumble876 <moyamckenzie142@gmail.com>
2026-01-20 20:05:14 -0500

    Add files via upload

---
+++ web-design-portfolio/css/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const getStartedButton = document.querySelector('.hero button');
+
+    getStartedButton.addEventListener('click', () => {
+        alert('Thank you for clicking! We will contact you soon.');
+    });
+});
